4b90d12c4bfd147787686f56ea08a7689fc02147
Daniel Hardman <dhardman@adaptivecomputing.com>
2013-10-04 09:18:28 -0600

    tweak install instructions

--- lib/config.py
+++ lib/config.py
@@ -10,9 +10,6 @@ MAIN_MODULE = os.path.split(sys.argv[0])[1]
 APP_VERSION = '1.0'
 
 CODE_HOME_REPO = 'git@github.com:dhh1128/git-mux.git'
-print(os.path.abspath(sys.argv[0]))
-print(__file__)
-print(os.path.abspath(__file__))
 BIN_FOLDER = os.path.abspath(os.path.join(os.path.dirname(os.path.abspath(__file__)), '..')).replace('\\', '/')
 
 # Verify that this code is running from an environment that
@@ -20,11 +17,11 @@ BIN_FOLDER = os.path.abspath(os.path.join(os.path.dirname(os.path.abspath(__file
 if BIN_FOLDER.endswith('/'):
     BIN_FOLDER = BIN_FOLDER[0:-1]
 
-APP_ROOT = os.path.abspath(os.path.join(BIN_FOLDER, '..')).replace('\\', '/')
-if APP_ROOT.endswith('/'):
-    APP_ROOT = APP_ROOT[0:-1]
+GMUX_ROOT = os.path.abspath(os.path.join(BIN_FOLDER, '..')).replace('\\', '/')
+if GMUX_ROOT.endswith('/'):
+    GMUX_ROOT = GMUX_ROOT[0:-1]
 
-CONFIG_FOLDER = os.path.join(APP_ROOT, 'etc')
+CONFIG_FOLDER = os.path.join(GMUX_ROOT, 'etc')
 CONFIG_FNAME = '%s.cfg' % APP_NAME
 CONFIG_FQPATH = os.path.join(CONFIG_FOLDER, CONFIG_FNAME)
 
--- setup.py
+++ setup.py
@@ -5,9 +5,9 @@ from lib import ui, config
 REQUIRED_LAYOUT_EXPLANATION = '''
 This app is designed to be used with the following folder layout:
 
-$APP_ROOT/bin   -- all the code for the app; where $MAIN_MODULE resides.
-$APP_ROOT/repos -- all the repos that we'll work with.
-$APP_ROOT/etc   -- config files.
+$GMUX_ROOT/bin   -- all the code for the app; where $MAIN_MODULE resides.
+$GMUX_ROOT/repos -- all the repos that we'll work with.
+$GMUX_ROOT/etc   -- config files.
 '''
 
 INSTALLATION_INSTRUCTIONS = '''
@@ -15,21 +15,28 @@ Installation instructions:
 
 1. Make a folder named "$APP_NAME" in any convenient location. The folder
    should have the capacity to store git repos. The fully qualified
-   path to this folder becomes $APP_ROOT. The path should be writable by
+   path to this folder becomes $GMUX_ROOT. The path should be writable by
    your non-root user.
-2. In a shell, cd to $APP_ROOT
+2. In a shell, cd to $GMUX_ROOT
 2.   git clone $CODE_HOME_REPO bin
-3.   sudo python bin/$MAIN_MODULE setup
+3.   sudo python bin/setup.py
 4. Answer the prompts.
 '''
+
+def report_step(step):
+    ui.printc('%s...' % step, ui.PARAM_COLOR)
+
+def complain(problem):
+    ui.eprintc(problem, ui.ERROR_COLOR)
 
 _non_root_user = None
 def get_non_root_user():
     global _non_root_user
     if _non_root_user is None:
-        _non_root_user = do_or_die('stat -c %%U %s' % config.APP_ROOT, explanation='Need to be able to figure out non-root user.').strip()
+        report_step('Finding non-root user')
+        _non_root_user = do_or_die('stat -c %%U %s' % config.GMUX_ROOT, explanation='Need to be able to figure out non-root user.').strip()
         if _non_root_user == 'root':
-            die('%s is owned by root. Needs to be owned by ordinary user.' % config.APP_ROOT)
+            die('%s is owned by root. Needs to be owned by ordinary user.' % config.GMUX_ROOT)
     return _non_root_user
 
 _installer = None
@@ -45,7 +52,7 @@ def get_installer():
     return _installer
 
 def die(msg):
-    ui.eprintc(msg, ui.ERROR_COLOR)
+    complain(msg)
     sys.exit(1)
 
 def do(cmd, as_user=None):
@@ -68,9 +75,9 @@ def do_or_die(cmd, as_user=None, explanation=None):
     return stdout
 
 def setup_folder_layout(audit_only=False):
-    ui.printc('Checking folder layout...', ui.PARAM_COLOR)
+    report_step('Checking folder layout')
     if not config.BIN_FOLDER.endswith('/bin'):
-        msg = '%s is not a valid location for $APP_ROOT/bin.\n' % config.BIN_FOLDER + \
+        msg = '%s is not a valid location for $GMUX_ROOT/bin.\n' % config.BIN_FOLDER + \
             REQUIRED_LAYOUT_EXPLANATION + INSTALLATION_INSTRUCTIONS
         msg = msg.replace('$APP_NAME', config.APP_NAME)
         msg = msg.replace('$BIN_FOLDER', config.BIN_FOLDER)
@@ -78,16 +85,14 @@ def setup_folder_layout(audit_only=False):
         msg = msg.replace('$MAIN_MODULE', config.MAIN_MODULE)
         msg = msg.strip()
         if audit_only:
-            eprintc(msg, ui.ERROR_COLOR)
-            return False
-        else:
-            die(msg)
+            return complain(msg)
+        die(msg)
     else:
         print('Folder layout is correct.')
     return True
 
 def setup_writable_app_folder(audit_only=False):
-    ui.printc('Checking for writable app folder...', ui.PARAM_COLOR)
+    report_step('Checking for writable app folder')
     nru = _get_non_root_user()
     tmp_path = os.path.join(APP_FOLDER, ".tmp" + str(time.time()))
     exit_code, stdout, stderr = do('touch %s' % tmp_path, as_user=nru)
@@ -95,75 +100,66 @@ def setup_writable_app_folder(audit_only=False):
         if exit_code:
             msg = '%s is not writable by %s.' % (config.APP_FOLDER, nru)
             if audit_only:
-                eprintc(msg, ui.ERROR_COLOR)
-                return False
-            else:
-                die(msg)
+                return complain(msg)
+            die(msg)
         else:
             print('%s is writable by %s.' % (config.APP_FOLDER, nru))
     finally:
         os.remove(tmp_path)
+    return True
 
 def setup_app_cloned(audit_only=False):
-    ui.printc('Checking app to see if it was installed by cloning a git repo...', ui.PARAM_COLOR)
+    report_step('Checking whether app was installed by cloning a git repo')
     if not os.path.isdir(os.path.join(config.BIN_FOLDER, '.git')):
         msg = '''
 %s has not been installed with a git clone command. This will prevent it
 from updating itself.
 ''' % config.APP_TITLE + config.INSTALLATION_INSTRUCTIONS
         if audit_only:
-            eprintc(msg, ui.ERROR_COLOR)
-            return False
-        else:
-            die(msg)
+            return complain(msg)
+        die(msg)
     else:
         print('%s was cloned correctly.' % config.APP_TITLE)
     return True
 
 def setup_git(audit_only=False):
-    ui.printc('Checking git...', ui.PARAM_COLOR)
+    report_step('Checking git')
     exit_code, stdout, stderr = do('git --version', as_user=get_non_root_user())
     if exit_code:
         if audit_only:
-            eprintc('Git is not installed.', ui.ERROR_COLOR)
-            return False
-        else:
-            print('Installing git.')
-            do_or_die('%s install git /y' % get_installer(), 'Need git to be installed.')
-            print('Try setup again, now that git is installed.')
-            sys.exit(1)
+            return complain('Git is not installed.')
+        print('Installing git.')
+        do_or_die('%s install git /y' % get_installer(), 'Need git to be installed.')
+        print('Try setup again, now that git is installed.')
+        sys.exit(1)
     else:
         print('Git is installed.')
     return True
 
 def setup_git_python(audit_only=False):
-    ui.printc('Checking gitpython...', ui.PARAM_COLOR)
+    report_step('Checking gitpython')
     try:
         import git as gitpython
         print('Gitpython is installed.')
     except:
         if audit_only:
-            eprintc('Gitpython is not installed.', ui.ERROR_COLOR)
-            return False
-        else:
-            print('Installing gitpython.')
-            do_or_die('easy_install gitpython', explanation='Need gitpython to be installed.')
+            return complain('Gitpython is not installed.')
+        print('Installing gitpython.')
+        do_or_die('easy_install gitpython', explanation='Need gitpython to be installed.')
     return True
 
 def setup_git_flow(audit_only=False):
-    ui.printc('Checking git-flow...', ui.PARAM_COLOR)
+    report_step('Checking git-flow')
     exit_code, stdout, stderr = do('which git-flow')
     if exit_code:
         if audit_only:
-            eprintc('Git-flow is not installed.', ui.ERROR_COLOR)
-            return False
-        else:
-            print('Installing git-flow.')
-            installer = get_installer()
-            pkg = 'gitflow'
-            if installer == 'apt-get':
-                pkg = 'git-flow'
-            do_or_die('%s install %s /y' % (installer, pkg), explanation='Need git-flow to be installed.')
+            return complain('Git-flow is not installed.')
+        print('Installing git-flow.')
+        installer = get_installer()
+        pkg = 'gitflow'
+        if installer == 'apt-get':
+            pkg = 'git-flow'
+        do_or_die('%s install %s /y' % (installer, pkg), explanation='Need git-flow to be installed.')
     else:
         print('Git-flow is installed.')
     return True
@@ -171,8 +167,10 @@ def setup_git_flow(audit_only=False):
 def run(audit_only=False):
     # Only run this as root. This is a backup check; it's also enforced
     # in gitmux.setup().
-    assert os.getuid() == 0
-    ui.printc('Checking user context...', ui.PARAM_COLOR)
+    if os.getuid() != 0:
+        die('Must run setup as root user.')
+    # Make sure we know who is the non-root user that's temporarily running
+    # with elevated privileges, so we can run other commands as that user.
     get_non_root_user()
     try:
         setup_git()
