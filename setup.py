13e4e48f3c1e34f4e3fc02edd1dbd0f35b2a6e73
Daniel Hardman <dhardman@adaptivecomputing.com>
2013-10-04 11:30:12 -0600

    more tweaks

--- setup.py
+++ setup.py
@@ -186,7 +186,7 @@ def setup_data():
 def update_app():
     git = gitpython.Git(config.BIN_FOLDER)
     exit_code, stdout, stderr = git.pull()
-    if exit_code or stdout.find('xx') > -1:
+    if exit_code or stdout.find('Unpacking objects') > -1:
         die('%s was out of date. Re-run setup with new version.' % config.APP_NAME)
 
 def run(audit_only=False):
